3326a253066a8e30ddfd61a5f5eeb951ade8a7ae
Alexey <alexsmirnovalex05@gmail.com>
2024-09-29 21:25:54 +0700

    Добавлена функция для удаления трека из плейлиста

--- SmirnovAlexeyPI-31LAB2/Header.h
+++ SmirnovAlexeyPI-31LAB2/Header.h
@@ -54,3 +54,12 @@ void add_song(Playlist* playlist, Track song) {
     playlist->tracks[playlist->total_number_of_tracks] = song;
     playlist->total_number_of_tracks++;
 }
+
+// Функция для удаления трека из плейлиста
+void remove_song(Playlist* playlist, int index) {
+    for (int i = index; i < playlist->total_number_of_tracks - 1; i++) {
+        playlist->tracks[i] = playlist->tracks[i + 1];
+    }
+    playlist->total_number_of_tracks--;
+    playlist->tracks = (Track*)realloc(playlist->tracks, playlist->total_number_of_tracks * sizeof(Track));
+}
