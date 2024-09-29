8eb7dd544faf5ed0e3bc2ec8789aa4013542bb74
Alexey <alexsmirnovalex05@gmail.com>
2024-09-29 21:22:21 +0700

    Добавлены структуры

--- SmirnovAlexeyPI-31LAB2/Header.h
+++ SmirnovAlexeyPI-31LAB2/Header.h
@@ -4,3 +4,46 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
+
+// Структура для хранения информации о треке
+typedef struct {
+    char title[100];
+} Track;
+
+// Структура для хранения плейлиста
+typedef struct {
+    Track* tracks;
+    int total_number_of_tracks; // Общее количество треков в текущем плейлисте
+    int current_track; // Текущий трек
+} Playlist;
+
+// Структура для хранения громкости
+typedef struct {
+    int volume;
+} Volume;
+
+// Структура для хранения воспроизведения
+typedef struct {
+    Playlist* playlist;
+    int index;
+} Playback;
+
+// Структура для хранения паузы
+typedef struct {
+    Playlist* playlist;
+} Pause;
+
+// Структура для хранения перехода к следующему треку
+typedef struct {
+    Playlist* playlist;
+} Skip;
+
+// Структура для хранения возврата к предыдущему треку
+typedef struct {
+    Playlist* playlist;
+} Previous;
+
+// Структура для хранения просмотра треков в плейлисте
+typedef struct {
+    Playlist* playlist;
+} View;
